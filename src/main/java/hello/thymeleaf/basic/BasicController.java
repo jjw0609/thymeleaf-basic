12c65c98415814598ceb4500ad19a7d55dcdd74f
jjw0609 <jjw_0609@naver.com>
2025-02-19 11:16:17 +0900

    반복, 조건부 평가, 주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -109,8 +109,34 @@ public class BasicController {
 
     @GetMapping("/each")
     public String each(Model model) {
+
         addUsers(model);
+
         return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+
+        addUsers(model);
+
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+
+        model.addAttribute("data", "Spring!");
+
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+
+        addUsers(model);
+
+        return "basic/block";
     }
 
     private void addUsers(Model model) {
