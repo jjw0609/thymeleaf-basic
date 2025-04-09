1446718bf7843200db55315c9cb032b893495bb0
wjdwodnd93 <jjw_0609@naver.com>
2025-04-09 21:37:12 +0900

    서블릿 예외 처리

---
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,22 @@
+package exception.src.main.java.hello.exception.servlet;
+
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생!");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
