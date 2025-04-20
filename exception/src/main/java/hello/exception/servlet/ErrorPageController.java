ffdb5e13de0a06395afc6ad21663b1936d6c8348
wjdwodnd93 <jjw_0609@naver.com>
2025-04-20 22:00:02 +0900

    API 예외 처리

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -40,8 +40,7 @@ public class ErrorPageController {
     }
 
     @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Object>> errorPage500Api(
-            HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
 
         log.info("API errorPage 500");
 
